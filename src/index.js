e75cfa72cda798b892f7e2d2e6f11637183c4c71
Christian Pfeiffer <cpfeiffer325@gmail.com>
2019-11-20 22:44:56 -0700

    update basic index page with avatar and blod post sample

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
 // Import React and ReactDOM libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// Create a react component
+
 const App = () => {
-  const buttonText = { text: 'Click me' };
-  const labelText = 'Enter name:';
-
   return (
-    <div>
-      <label className="label" htmlFor="name">
-        {labelText}
-      </label>
-      <input id="name" type="text" />
-      <button style={{ backgroundColor: 'blue', color: 'white' }}>
-        {buttonText.text}
-      </button>
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar "/>
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Lilybug
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00PM</span>
+          </div>
+          <div className="text">Nice blog post1</div>
+        </div>
+      </div>
     </div>
-  );
-};
+  )
+}
 
 // Take the react component and render on page
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'))
