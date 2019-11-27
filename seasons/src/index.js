8d0a25436484b7d097852bb03ee57ed1d28c4d72
Christian Pfeiffer <cpfeiffer325@gmail.com>
2019-11-26 22:52:06 -0700

    refactor state and add SeasonalDisplay component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// import SeasonDisplay from './SeasonDisplay'
+import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-
-    // this is the only time we do direct assignment to this.state
-    this.state = { lat: null, errorMessage: '' }
-  }
+  state = { lat: null, errorMessage: '' }
   
   componentDidMount() {    
     window.navigator.geolocation.getCurrentPosition(
@@ -24,7 +19,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>
+      return <SeasonDisplay lat={this.state.lat}/>
     }
 
     return <div>Loading!</div>
