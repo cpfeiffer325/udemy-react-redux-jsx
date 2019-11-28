d54bbd173361a441daa197033f28641498ad233f
Christian Pfeiffer <cpfeiffer325@gmail.com>
2019-11-27 23:07:13 -0700

    add helper function to render red boarder

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -13,17 +13,21 @@ class App extends React.Component {
       err => this.setState({ errorMessage: err.message })
     )
   }
-  
-  render() {
+
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>
     }
-
+  
     if (!this.state.errorMessage && this.state.lat) {
       return <SeasonDisplay lat={this.state.lat}/>
     }
-
+  
     return <Loading message="Please accept location request"/>
+  }
+
+  render() {
+    return <div className="border-red">{this.renderContent()}</div>
   }
 }
 
