d2c87a7666514441097f7386d8c120dd600595b2
Christian Pfeiffer <cpfeiffer325@gmail.com>
2019-12-04 22:20:19 -0700

    add state to response and add output of number of images  returned

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -5,19 +5,22 @@ import SearchBar from './SearchBar'
 
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
+  state = { images: [] }
+
+  onSearchSubmit = async (term) => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
         Authorization: 'Client-ID 2d2767d8962b5064f338741a6d554e823e3a75656bb953d58dfbef7bf31ee1ab'
       }
     })
-    console.log(response.data.results)
+    this.setState({ images: response.data.results })
   }
   render () {
     return (
     <div className="ui container" style={{ marginTop: '10px' }}>
-      <SearchBar onSubmit={this.onSearchSubmit}/>  
+      <SearchBar onSubmit={this.onSearchSubmit}/>
+      Found: {this.state.images.length} images
     </div>
     )
 
