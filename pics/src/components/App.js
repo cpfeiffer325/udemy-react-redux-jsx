20005d3adddc1a2135ae4d39b1aadf7946f455ee
Christian Pfeiffer <cpfeiffer325@gmail.com>
2019-12-04 22:04:59 -0700

    create get request from unsplash api for photos

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import axios from 'axios'
 
 import SearchBar from './SearchBar'
 
+
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term)
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID 2d2767d8962b5064f338741a6d554e823e3a75656bb953d58dfbef7bf31ee1ab'
+      }
+    })
   }
   render () {
     return (
