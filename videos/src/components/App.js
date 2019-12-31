3bfec3de5117c1f6a46c4023b3b5e72c152402e8
Christian Pfeiffer <cpfeiffer325@gmail.com>
2019-12-31 10:54:24 -0700

    create default video selection on page load

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -5,37 +5,44 @@ import VideoList from './VideoList'
 import VideoDetail from './VideoDetail'
 
 class App extends React.Component {
-  state = { 
+  state = {
     videos: [],
     selectedVideo: null
   }
-  
+
+  componentDidMount() {
+    this.onTermSubmit('movies')
+  }
+
   onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
       params: {
         q: term
       }
     })
-    
-    this.setState({ videos: response.data.items })
+
+    this.setState({ 
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    })
   }
 
   onVideoSelect = (video) => {
-    this.setState({selectedVideo: video})
+    this.setState({ selectedVideo: video })
   }
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit}/>
+        <SearchBar onFormSubmit={this.onTermSubmit} />
         <div className="ui grid">
           <div className="ui row">
             <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo}/>
+              <VideoDetail video={this.state.selectedVideo} />
             </div>
             <div className="five wide column">
               <VideoList
-                onVideoSelect={this.onVideoSelect} 
+                onVideoSelect={this.onVideoSelect}
                 videos={this.state.videos}
               />
             </div>
