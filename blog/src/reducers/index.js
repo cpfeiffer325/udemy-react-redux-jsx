6a84e967bcfc6b6fe14ab57414b5bb014afefcf5
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-01-02 21:25:06 -0700

    create posts reducer

--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,8 +1,7 @@
 import { combineReducers } from 'redux'
+import postsReducer from './postsReducer'
 
 // Reducers
-
-
 export default combineReducers({
-  replaceMe: () => 'hi hi hi'
+  posts: postsReducer
 })
