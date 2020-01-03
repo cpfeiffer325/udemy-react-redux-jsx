ff5bcf6d651ac238077699157c889adcc25e17f4
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-01-02 22:14:48 -0700

    map state to props for fetched posts

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -4,6 +4,6 @@ import jsonPlaceholer from '../apis/jsonPlaceholder'
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholer.get('/posts')
   
-  dispatch({ type: 'FETCH_POSTS', payload: response })
+  dispatch({ type: 'FETCH_POSTS', payload: response.data })
 }
 
