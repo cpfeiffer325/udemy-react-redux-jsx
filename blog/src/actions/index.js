efadb8dea9aba300c7a096f2f11d504ac2d5cf7b
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-01-05 22:45:01 -0700

    add fetch user action

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,9 +1,15 @@
-import jsonPlaceholer from '../apis/jsonPlaceholder'
+import jsonPlaceholder from '../apis/jsonPlaceholder'
 
 // Action Creator
 export const fetchPosts = () => async dispatch => {
-  const response = await jsonPlaceholer.get('/posts')
+  const response = await jsonPlaceholder.get('/posts')
   
   dispatch({ type: 'FETCH_POSTS', payload: response.data })
+}
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`)
+  
+  dispatch({ type: 'FETCH_USER', payload: response.data })
 }
 
