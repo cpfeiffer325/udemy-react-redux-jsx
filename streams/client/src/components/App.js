86c67a52c9eefd1b40fb333b444f587fa4cc37d0
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-01-07 22:55:02 -0700

    create stream components

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <BrowserRouter>
+    
+    </BrowserRouter>
+  )
 }
 
 export default App
