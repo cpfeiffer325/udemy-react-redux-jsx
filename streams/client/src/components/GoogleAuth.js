03433a8dec17a2890ce321b4cab8139938e1036e
cpfeiffer325 <cpfeiffer325@gmail.com>
2020-01-12 17:19:14 -0700

    create sign in sign out logic

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,17 +1,32 @@
 import React from 'react'
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null }
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
         clientId: '327313692543-fnrjc451a0h536t6lv3ag3a81eo9gjos.apps.googleusercontent.com',
         scope: 'email'
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance()
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
       })
     })
   }
   
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know if we are signed in</div>
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed In!</div>
+    } else {
+      return <div>I am not signed in</div>
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>
+    return <div>{this.renderAuthButton()}</div>
   }
 }
 
