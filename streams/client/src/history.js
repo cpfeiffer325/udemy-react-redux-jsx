7ec9c93ed3daa7c49bb48fbdc30583c99710b3a5
cpfeiffer325 <cpfeiffer325@gmail.com>
2020-01-19 10:22:32 -0700

    switch from browserRouter to plain router to manage history

---
+++ streams/client/src/history.js
@@ -0,0 +1,6 @@
+import { createBrowserHistory } from 'history' 
+export default createBrowserHistory()
+
+// below syntax will cause a browser error, switch to above syntax
+// import createHistory from 'history/createBrowserHistory' 
+// export default createHistory()
