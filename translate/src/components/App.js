7489713995ac2746f402a47217829e46648a6478
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-01-24 23:54:05 -0700

    update App component for language selectors and to create a user

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import UserCreate from './UserCreate'
 
 class App extends React.Component {
+  state = { language: 'english' }
+
+  onLanguageChange = language => {
+    this.setState({ language })
+  }
+
   render() {
-    return <div>App</div>
+    return (
+      <div>
+        <div className="ui container">
+          Select a language:
+          <i onClick={() => this.onLanguageChange('english')}  className="flag us" />
+          <i onClick={() => this.onLanguageChange('dutch')}  className="flag nl" />
+          <i onClick={() => this.onLanguageChange('german')}  className="flag de" />
+          <i onClick={() => this.onLanguageChange('english')}  className="flag ca" />
+        </div>
+        <UserCreate />
+      </div>
+    )
   }
 }
 
