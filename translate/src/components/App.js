233a27a77fc4af2b17e4c771be4cac7766f0c0d9
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-01-26 22:28:57 -0700

    create and connect language store to App

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,27 +1,20 @@
 import React from 'react'
 import UserCreate from './UserCreate'
-import LanguageContext from '../contexts/LanguageContext'
+import { LanguageStore } from '../contexts/LanguageContext'
 import ColorContext from '../contexts/ColorContext'
-import languageSelector from './languageSelector'
-import LanguageSelector from './languageSelector'
+import LanguageSelector from './LanguageSelector'
 
 class App extends React.Component {
-  state = { language: 'english' }
-
-  onLanguageChange = language => {
-    this.setState({ language })
-  }
-
   render() {
     return (
       <div>
-        <LanguageSelector onLanguageChange={this.onLanguageChange} />
-        
-        <ColorContext.Provider value="red">
-          <LanguageContext.Provider value={this.state.language}>
-              <UserCreate />
-          </LanguageContext.Provider>
-        </ColorContext.Provider>
+        <LanguageStore>
+          <LanguageSelector />
+          
+          <ColorContext.Provider value="red">
+            <UserCreate />
+          </ColorContext.Provider>
+        </LanguageStore>
       </div>
     )
   }
