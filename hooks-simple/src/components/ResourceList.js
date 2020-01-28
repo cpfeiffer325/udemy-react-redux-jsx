13e7c5cd1d1f94037b4b15efcf6b10eb1fd168b5
Christian Pfeiffer <cpfeiffer325@gmail.com>
2020-01-27 21:41:17 -0700

    refactor and create reusable componenet for useResources function

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,17 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const ResourceList = ({ resource }) => {
+const useResources = (resource) => {
   const [resources, setResources] = useState([])
-  const fetchResource = async (resource) => {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
 
-    setResources(response.data)
-  }
 
-  useEffect(() => {
-    fetchResource(resource)
-  }, [resource])
+  useEffect(
+    () => {
+      (async resource => {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+    
+        setResources(response.data)
+      })(resource)
+    },
+    [resource]
+  )
+  return resources
+}
+
+const ResourceList = ({ resource }) => {
+  const resources = useResources(resource)
 
   return (
     <ul>
